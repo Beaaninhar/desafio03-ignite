ab285b6438c0502fd24d2604fcbebde3df8a3a5f
ana.beatriz <ana.beatriz@m4u.com.br>
2021-10-16 13:16:34 -0300

    Feito estilizaçao da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
 import Head from 'next/head';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -32,10 +36,67 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Home | spacetraveling</title>
       </Head>
 
-      <h1>Olá, mundo!</h1>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.postsContainer}>
+          <Link href="#">
+            <a className={styles.postContent}>
+              <strong>Como utilizar Hooks</strong>
+              <span>Pensando em sincronização em vez de ciclos de vida.</span>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Joseph Oliveira
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <Link href="#">
+            <a className={styles.postContent}>
+              <strong>Como utilizar Hooks</strong>
+              <span>Pensando em sincronização em vez de ciclos de vida.</span>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Joseph Oliveira
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <Link href="#">
+            <a className={styles.postContent}>
+              <strong>Como utilizar Hooks</strong>
+              <span>Pensando em sincronização em vez de ciclos de vida.</span>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Joseph Oliveira
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">Carregar mais posts</button>
+        </div>
+
+      </main>
     </>
   );
 }
